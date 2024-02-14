9cb520e92fe1b72a1d9b2e72e371a4e845e1718f
LeonardHolter <leonardholter17@outlook.com>
2024-02-14 16:00:39 +0100

    last update

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -16,7 +16,8 @@ struct ContentView: View {
     @State private var tipPercentage = 20
     @FocusState private var amoutIsFocused: Bool
     
-    let tipPercentages = [0, 10, 15, 20, 25]
+    
+    
     
     var totalPerPerson: Double {
         //calculate the total per person here
@@ -28,7 +29,16 @@ struct ContentView: View {
         let amountPerPerson = grandTotal / peopleCount
         
         return amountPerPerson
+    
+   
+
     }
+    
+    var totalCheck: Double {
+            let total = Double(checkAmount)
+            let percentage = tipPercentage
+        return Double(total + total/100 * Double(percentage))
+        }
     
     var body: some View {
         NavigationStack {
@@ -49,14 +59,18 @@ struct ContentView: View {
                 Section("How much do you want to tip?") {
                     
                     Picker("Tip percentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages, id: \.self) {
+                        ForEach(1..<100, id: \.self) {
                             Text($0, format: .percent)
                         }
                     }
-                    .pickerStyle(.segmented)
+                    .pickerStyle(.navigationLink)
                 }
                 
-                Section {
+                Section("Entire Check") {
+                    Text(totalCheck, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
+                Section("Amount Per Person") {
                     Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
             }
